f725b4526968843737e09adb29d8bf3d0ff9ee71
Bharath kumar G 39 bca <bharathkumarg668@gmail.com>
2025-02-05 23:13:23 +0530

    debugged the cors

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173", 
-        credentials: true,
+      origin: "*", 
+      methods: "GET,POST,PUT,DELETE",
+      allowedHeaders: "Content-Type,Authorization",
     })
-);
+  );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
